9d692e6794a9e35a92fb08c79aacfff2fc3a3a2f
tgibbs0318-max <tgibbs0318@gmail.com>
2026-05-06 15:35:48 -0400

    Add files via upload

---
+++ Portfolio Project - EDA.sql
@@ -0,0 +1,177 @@
+-- Exploratory Data Analysis
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+-- EASIER QUERIES
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM world_layoffs.layoffs_staging2;
+
+-- percentage laidoff is 100%
+
+SELECT * 
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC; 
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC; 
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC; 
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+-- SOMEWHAT TOUGHER AND MOSTLY USING GROUP BY
+
+-- Companies with the biggest single Layoff
+
+SELECT company, total_laid_off
+FROM world_layoffs.layoffs_staging2
+ORDER BY 2 DESC
+LIMIT 5;
+
+
+-- Companies with the most Total Layoffs
+SELECT company, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- by location
+SELECT location, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- this it total in the past 3 years or in the dataset
+
+SELECT country, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT YEAR(date), SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY YEAR(date)
+ORDER BY 1 DESC;
+
+
+SELECT industry, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+SELECT stage, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+-- SLIGHTLY TOUGHER QUERIES
+
+-- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year. It's a little more difficult.
+-- Using CTEs
+
+WITH Company_Year AS 
+(
+  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+  FROM layoffs_staging2
+  GROUP BY company, YEAR(date)
+)
+, Company_Year_Rank AS (
+  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+  FROM Company_Year
+)
+SELECT company, years, total_laid_off, ranking
+FROM Company_Year_Rank
+WHERE ranking <= 5
+AND years IS NOT NULL
+ORDER BY years ASC, total_laid_off DESC;
+
+
+-- Rolling Total of Layoffs Per Year-Month
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+WHERE SUBSTRING(date,1,7) IS NOT NULL
+GROUP BY dates
+ORDER BY dates ASC;
+
+-- now use it in a CTE so we can query off of it
+WITH Rolling_Total AS 
+(
+SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY dates
+ORDER BY dates ASC
+)
+SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
+FROM Rolling_Total
+ORDER BY dates ASC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
